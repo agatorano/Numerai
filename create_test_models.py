1b00dac6edab701faf4dec0170b4fc62f6b502c5
Andre Gatorano <agatorano@gmail.com>
2015-12-30 09:54:16 -0600

    added rank averagin

--- create_test_models.py
+++ create_test_models.py
@@ -3,11 +3,14 @@ from collections import Counter
 import pandas as pd
 import numpy as np
 
+from scipy import stats
+
 from sklearn.ensemble import RandomForestClassifier as RF
 from sklearn.linear_model import LogisticRegression as LR
 from sklearn.grid_search import GridSearchCV
 from sklearn.svm import SVC
 
+from sklearn import preprocessing
 from sklearn.metrics import roc_auc_score
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
@@ -51,19 +54,38 @@ def optimize_model(model, x_train, y_train, x_val, y_val, parameters):
     return clf
 
 
-def voting_ensemble_prediction(model_list, x_val, y_val):
+def get_prediction_df(model_list, x_val, type='auc'):
     '''
-    voting based prediction for multiple models
-    find most common label for each observation
+    get a dataframe of predictions
+    defaults to returning a df of probability scores
+    each column is a model
     '''
 
     predictions = []
     for model in model_list:
-        pred = model.predict(x_val.as_matrix())
-        pred = pd.Series(pred)
-        predictions.append(pred)
+
+        if type == 'auc':
+            pred_auc = model.predict_proba(x_val.as_matrix())[:, 1]
+            pred_auc = pd.Series(pred_auc)
+            predictions.append(pred_auc)
+        else:
+            pred = model.predict(x_val.as_matrix())
+            pred = pd.Series(pred)
+            predictions.append(pred)
 
     predictions = pd.concat(predictions, axis=1)
+
+    return predictions
+
+
+def voting_ensemble_prediction(model_list, x_val, y_val):
+    '''
+    create a voting ensemble from a model list
+    unweighted domacracy vote
+    '''
+
+
+    predictions = get_prediction_df(model_list, x_val, type='pred')
 
     results = []
 
@@ -75,24 +97,43 @@ def voting_ensemble_prediction(model_list, x_val, y_val):
 
     print(accuracy_score(y_val.as_matrix(), results))
 
-    return results
 
-
-def averaging_ensemble_prediction(model_list, x_val, y_val):
+def averaging_ensemble_prediction(model_list, x_val, y_val, ranks=None):
     '''
-    average probability scores from multiple models
+    averages probability scores of multiple models
+    smooths out decision boundery of ensemble
+    if ranks are sent average rank is determined
     '''
 
-    predictions = []
-    for model in model_list:        
-        pred_auc = model.predict_proba(x_val.as_matrix())[:,1]
-        pred_auc = pd.Series(pred_auc)
-        predictions.append(pred_auc)
-
-    predictions = pd.concat(predictions, axis=1)
-    pred_auc = predictions.mean(axis=1)
+    if ranks is not None:
+        ranks = ranks.mean(axis=1)
+        pred_auc = preprocessing.normalize(ranks)[0]
+    else:
+        predictions = get_prediction_df(model_list, x_val)
+        pred_auc = predictions.mean(axis=1)
 
     print(roc_auc_score(y_val.as_matrix(), pred_auc))
+
+    return pred_auc
+
+
+def rank_averaging_prediction(model_list, x_val, y_val):
+    '''
+    creates dataframe of probability score ranks
+    sends the data frame to avaraging_ensemble_prediction
+    for auc scoring
+    '''
+
+    predictions = get_prediction_df(model_list, x_val)
+
+    ranks = []
+    for col in predictions.columns:
+        rank = pd.Series(stats.rankdata(predictions[col]))
+        ranks.append(rank)
+
+    ranks = pd.concat(ranks, axis=1)
+
+    pred_auc = averaging_ensemble_prediction(model_list, x_val, y_val, ranks)
 
     return pred_auc
 
